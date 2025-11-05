caa58baac4dbca46c820736f06dbd486f8a5758a
AQUILES\aquiles <aquileslorca15@gmail.com>
2025-11-05 22:01:03 +0100

    Prueba perfiles

--- index.js
+++ index.js
@@ -6,24 +6,175 @@ import cors from "cors";
 let api = 'RGAPI-5511e4bb-2454-4751-b434-d26a521ea2f2';
 let users = [];
 
+const divisiones = [
+  ['IRON', 'IV'],
+  ['IRON', 'III'],
+  ['IRON', 'II'],
+  ['IRON', 'I'],
+
+  ['BRONZE', 'IV'],
+  ['BRONZE', 'III'],
+  ['BRONZE', 'II'],
+  ['BRONZE', 'I'],
+
+  ['SILVER', 'IV'],
+  ['SILVER', 'III'],
+  ['SILVER', 'II'],
+  ['SILVER', 'I'],
+
+  ['GOLD', 'IV'],
+  ['GOLD', 'III'],
+  ['GOLD', 'II'],
+  ['GOLD', 'I'],
+
+  ['PLATINUM', 'IV'],
+  ['PLATINUM', 'III'],
+  ['PLATINUM', 'II'],
+  ['PLATINUM', 'I'],
+
+  ['EMERALD', 'IV'],
+  ['EMERALD', 'III'],
+  ['EMERALD', 'II'],
+  ['EMERALD', 'I'],
+
+  ['DIAMOND', 'IV'],
+  ['DIAMOND', 'III'],
+  ['DIAMOND', 'II'],
+  ['DIAMOND', 'I'],
+];
+
 const sleep = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function getListadoPorRango(rango, minirango, page) {
-  const url = `https://euw1.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/${rango}/${minirango}?page=${page}&api_key=${api}`;
+  const convertirMilisegundosAFecha = (milisegundos) => {
+    // Crear un objeto Date con los milisegundos
+    const fecha = new Date(milisegundos);
 
+    // Formatear la fecha
+    const dia = fecha.getDate().toString().padStart(2, '0');
+    const mes = (fecha.getMonth() + 1).toString().padStart(2, '0');
+    const anio = fecha.getFullYear();
+    const horas = fecha.getHours().toString().padStart(2, '0');
+    const minutos = fecha.getMinutes().toString().padStart(2, '0');
+    const segundos = fecha.getSeconds().toString().padStart(2, '0');
+
+    return `${dia}/${mes}/${anio} ${horas}:${minutos}`;
+  }
+
+const comprobarFechaPartidaValidaEnRango = (fechaPartidaNum, dias) => {
+  const fechaActual = new Date();
+  const fechaPartida = new Date(fechaPartidaNum);
+
+  // 24 horas * 60 minutos * 60 segundos * 1000 milisegundos * número de días
+  const MILISEGUNDOS_EN_UN_DIA = 24 * 60 * 60 * 1000 * dias;
+
+  // Diferencia de tiempo en milisegundos entre hoy y la fecha de la partida
+  const diferenciaTiempo = fechaActual.getTime() - fechaPartida.getTime();
+
+  // Devuelve true si la diferencia es menor o igual al rango indicado
+  return diferenciaTiempo <= MILISEGUNDOS_EN_UN_DIA;
+}
+
+async function refrescarJugadores(contadoListaRefrescar) {
+let terminado = false;
+let contador = 1;
+  while(!terminado) {
+    let perfiles = await getListadoPorRango(divisiones[contadoListaRefrescar][0], divisiones[contadoListaRefrescar][1], contador++)
+    if (perfiles && perfiles?.length) {
+      console.log(divisiones[contadoListaRefrescar][0], divisiones[contadoListaRefrescar][1], contador, perfiles[0].puuid)
+      perfiles.forEach(async (perfil) => {
+        let cantidadPartidas = perfil.wins + perfil.losses;
+        if (cantidadPartidas >= 100) {
+          let partidasJugador = await getPartidasJugador(perfil.puuid, cantidadPartidasMiradas)
+          if (partidasJugador && partidasJugador.length >= cantidadPartidasMiradas) {
+            let primeraPartida = await getDatosPartida(partidasJugador[0])
+
+            if (primeraPartida && primeraPartida.info) {
+              if (comprobarFechaPartidaValidaEnRango(primeraPartida.info.gameStartTimestamp, cantidadMinimaDias)) {
+                let datosJugador = await getDatosJugador(perfil.puuid);
+                if (datosJugador) {
+                  let wr = (cantidadPartidas > 0 ? (perfil.wins / cantidadPartidas) * 100 : 0)
+                  users.push({
+                    nick: datosJugador.gameName + '#' + datosJugador.tagLine,
+                    fechaUltimaPartida: primeraPartida.info.gameEndTimestamp,
+                    fechaUltimaPartidaString: convertirMilisegundosAFecha(primeraPartida.info.gameStartTimestamp),
+                    tier: perfil.tier,
+                    rank: perfil.rank,
+                    cantidadPartidas: cantidadPartidas,
+                    wins: perfil.wins,
+                    losses: perfil.losses,
+                    wr: parseFloat(wr.toFixed(2))
+                  })
+
+                }
+              }
+            }
+          }
+        }
+      });
+    } else {
+      terminado = true;
+      console.log(divisiones.length, contadoListaRefrescar)
+      if ((divisiones.length -1) > contadoListaRefrescar){
+        await refrescarJugadores(contadoListaRefrescar + 1)
+      } 
+    }
+  } 
+}
+
+
+async function getDatosJugador(puuid) {
+  const url = `https://europe.api.riotgames.com/riot/account/v1/accounts/by-puuid/${puuid}?api_key=${api}`;
   try {
     const response = await fetch(url);
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-
     const data = await response.json();
-    return data; // ✅ devuelve los datos correctamente
+    return data;
   } catch (error) {
-    console.error('❌ Error obteniendo datos de Riot API:', error);
-    return []; // devuelve array vacío para no romper el flujo
+    return []; 
   }
 }
+
+async function getListadoPorRango(rango, minirango, page) {
+  const url = `https://euw1.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/${rango}/${minirango}?page=${page}&api_key=${api}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return []; 
+  }
+}
+
+async function getPartidasJugador(puuid, cantidadPartidasMiradas) {
+  const url = `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=${cantidadPartidasMiradas}&queue=420&type=ranked&api_key=${api}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return []; 
+  }
+}
+
+
+async function getDatosPartida(idPartida) {
+  const url = `https://europe.api.riotgames.com/lol/match/v5/matches/${idPartida}?api_key=${api}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return []; 
+  }
+}
+
+
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -43,60 +194,15 @@ app.get("/perfiles", async (req, res) => {
   }
 });
 
-// app.get("/books/:id", (req, res) => {
-//   const data = readData();
-//   const id = parseInt(req.params.id);
-//   const book = data.books.find((book) => book.id === id);
-//   res.json(book);
-// });
 
-// app.post("/books", (req, res) => {
-//   const data = readData();
-//   const body = req.body;
-//   const newBook = {
-//     id: data.books.length + 1,
-//     ...body,
-//   };
-//   data.books.push(newBook);
-//   writeData(data);
-//   res.json(newBook);
-// });
+// FILTROS
+const cantidadMinimaPartidas = 100;
+const cantidadPartidasMiradas = 3;
+const cantidadMinimaDias = 7; //SE PERMITE HOY + 1 DIA
 
-// app.put("/books/:id", (req, res) => {
-//   const data = readData();
-//   const body = req.body;
-//   const id = parseInt(req.params.id);
-//   const bookIndex = data.books.findIndex((book) => book.id === id);
-//   data.books[bookIndex] = {
-//     ...data.books[bookIndex],
-//     ...body,
-//   };
-//   writeData(data);
-//   res.json({ message: "Book updated successfully" });
-// });
-
-// app.delete("/books/:id", (req, res) => {
-//   const data = readData();
-//   const id = parseInt(req.params.id);
-//   const bookIndex = data.books.findIndex((book) => book.id === id);
-//   data.books.splice(bookIndex, 1);
-//   writeData(data);
-//   res.json({ message: "Book deleted successfully" });
-// });
-
-app.listen(3000, async () => {
+app.listen(3001, async () => {
   console.log("Server listening on port 3000");
-  for (let index = 1; index <= 680; index++) {
-    await sleep(2000);
-    let perfiles = await getListadoPorRango('IRON', 'IV', index)
-    console.log(perfiles)
-    if (perfiles && perfiles?.length) {
-      perfiles.forEach(perfil => {
-        users.push(perfil);
-      });
-    }
-    
-  }
+  await refrescarJugadores(0)
 });
 
 
